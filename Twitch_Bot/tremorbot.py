faf39f5540f0999e78af1aa184af139ff74db3f0
TremorAi <tremor.liad@gmail.com>
2018-11-02 17:12:58 -0600

    adding functions

--- Twitch_Bot/tremorbot.py
+++ Twitch_Bot/tremorbot.py
@@ -7,6 +7,20 @@ class TwitchBot:
         self.username = username
         self.password = password
         self.channel = channel
+        
+        host = "irc.chat.twitch.tv"
+        port = 6667
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
+    
+    def sendauth(self):
+        return
+    
+    def start(self):
+        self.sendauth()
+        data = self.socket.recv(1024)
     
     def joinchannel(self):
+        
         return
