3b0fc28cbabaa825402e1fef5f07615f401462ea
TremorAi <tremor.liad@gmail.com>
2018-11-03 16:40:20 -0600

    added challenges

--- day4/stream_challenges.py
+++ day4/stream_challenges.py
@@ -1,8 +1,20 @@
-min = 1
-max = 50000
+min = 110
+max = 100000
 sum = 0
+half = 0
 
+# challenge 1 solved easy way
 for n in range(min, max+1):
     sum+=n
 
 print(str(sum))
+
+#better method
+# 1 2 3 4 5 6 7 8 9 10
+# 1 2    +        9 10 = 11
+half = max / 2
+sum = half * max + half
+print(int(sum * 5))
+
+
+
