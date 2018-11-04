3df8a272f0215c820805a9782e32c5528578d874
TremorAi <tremor.liad@gmail.com>
2018-11-04 02:04:27 -0700

    Added comments briefly describing how the functions work

--- Twitch_Bot/tremorbot.py
+++ Twitch_Bot/tremorbot.py
@@ -8,12 +8,16 @@ from time import strftime
 
 class TwitchBot(irc.bot.SingleServerIRCBot):
     def __init__(self, username, password, channel):
+
+        #setup of all variables used
         self.username = username
         self.password = password
         self.channel = channel
         self.project = "Twitch bot"
         self.time_format = "%H:%M:%S"
         self.admins_and_mods = ["tremorai", "userman2", "tremorbot"]
+
+        #command dict
         self.commands = {
             "github":self.bot_command("https://github.com/TremorAi/LearningPython", "github"),
             "discord":self.bot_command("https://discord.gg/UU3v4Ra", "discord"),
@@ -23,34 +27,34 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             "time":self.bot_command(None, "time"),
             "uptime":self.bot_command(strftime(self.time_format), "uptime")
             }
-        
-
-        url = 'https://api.twitch.tv/kraken/users?login=' + channel
+       
+       #Setup and use of variables to connect the bot
         host = "irc.chat.twitch.tv"
         port = 6667
         token = config.password
         username = config.username
         irc.bot.SingleServerIRCBot.__init__(self ,[(host, port, 'oauth:'+token)], self.username, self.username)
 
-        # self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # self.socket.connect((host, port))
-    
+    # The on_welcome function is ran for the bot to connect to the channel that it will be chatting in
     def on_welcome(self, c, e):
         c.join(self.channel)
         print(f"joining {self.channel} ...")
 
+    # no_permission is simply a function to send a message to the chat if the user doesnt have permission to use a command, letting them know that they don't
     def no_permission(self, nick, cmd, c):
         c.privmsg(self.channel, f"{nick} this user doesnt have the permission to use {cmd}")
 
+    # notfound is the function ran when a command is not found inside the dict
     def notfound(self):
         def command(c, _, whatever):
             self.sendmessage(c, "command not found.")
         return command
-           
+
+    # This function sends a message the channel with a string whenever its called        
     def sendmessage(self, c, saystring):
         c.privmsg(self.channel, saystring)
 
-
+    # bot_command is ran from the dict with instructions on what to do with the cmd 
     def bot_command(self, stringthing, cmd):
         def command(c, nick, args):
 
@@ -67,7 +71,8 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
                 self.sendmessage(c, stringthing)
 
         return command
-        
+
+    # admin_command is ran from the dict for admin commands, it checks to make sure the user is a admin (by using a list) than uses the instruction to do a command    
     def admin_command(self, stringthing, cmd):
         def command(c, nick, args):
             if nick in self.admins_and_mods:
@@ -75,7 +80,8 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
                     self.project = args[0][len(cmd)+1:]
                     print(self.project)
         return command
-                
+
+    # on_pubmsg checks the channel chat to see if the first letter is ! and if it is, the command is passed through do_command            
     def on_pubmsg(self, c, e):
         if e.arguments[0][:1] == '!':
             cmd = e.arguments[0].split(' ')[0][1:]
@@ -83,6 +89,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             self.do_command(e, cmd)
         return
 
+    # do_command takes the command passed from on_pubmsg and checks to see if it is in the dict, if so passes c, nick, e.arguments to it, if not they get passed to notfound
     def do_command(self, e, cmd):
         nick = e.source.nick
         c = self.connection
