d12a7b326d4b780a7e74672311549890dd3307fb
TremorAi <tremor.liad@gmail.com>
2018-11-10 18:43:19 -0700

    simple grid in tk

--- day11/gui.py
+++ day11/gui.py
@@ -2,19 +2,21 @@ import tkinter as tk
 import random
 
 class Application(tk.Frame):
+    hi_there = None
     def __init__(self, master=None):
         super().__init__(master)
+        self.canvas_width = 640
+        self.canvas_height = 480
+        self.w = tk.Canvas(master,width=self.canvas_width,height=self.canvas_height)
         self.master = master
+        self.draw()
         self.pack()
-        self.create_widgets()
-        self.choice = ["top","bottom","left","right"]
-    
-    def create_widgets(self):
-        self.hi_there = tk.Button(self, text="hey", command=self.say_hi)
-        self.hi_there.pack(side="top")
-    def say_hi(self):
-        self.guy_there = tk.Button(self, text="hey", command=self.say_hi)
-        self.guy_there.pack(side=random.choice(self.choice))
+        self.w.pack()
+    def draw(self):
+        self.w.create_rectangle(0, 0, self.canvas_width, self.canvas_height, fill="#476042")
+        for i in range(20):
+            self.w.create_line(i*20, 0, i*20, self.canvas_height)
+            self.w.create_line(0, i*20, self.canvas_width, i*20)
 
 root = tk.Tk()
 app = Application(master=root)
